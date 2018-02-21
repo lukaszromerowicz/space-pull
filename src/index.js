e793be4d115e9fef49d0ab463beca27cc5b39326
Lukasz Romerowicz <lukaszromerowicz@gmail.com>
2018-02-21 18:34:20 +0000

    Added git pull detection to trigger animation only then.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
 import React, { Component } from 'react'
 import Spaceship from './Spaceship'
 
+
+const detectGitPull = (data) => {
+  const pattern = /^Updating [ ]{0,}([a-z0-9A-Z]+\.{2,3}[a-z0-9A-Z]+)|(\[[a-z0-9A-Z]+\.{2,3}[a-z0-9A-Z]+\])$/
+  const antiPattern = /error/
+  
+  return pattern.test(data) && !antiPattern.test(data)
+}
+
+export const middleware = (store) => (next) => (action) => {
+  if ('SESSION_ADD_DATA' === action.type) {
+    const { data } = action;
+
+    if (detectGitPull(data)) {
+      store.dispatch({
+        type: 'TOGGLE_PULL_ROCKET'
+      })
+    }
+
+    next(action)
+  } else {
+    next(action)
+  }
+}
+
+export function reduceUI(state, action) {
+  switch (action.type) {
+    case 'TOGGLE_PULL_ROCKET': {
+      if (state.pullRocket !== undefined && state.pullRocket >= 0) {
+        return state.set('pullRocket', state.pullRocket + 1)
+      } else {
+        return state.set('pullRocket', 0)
+      }
+    }
+  }
+  return state
+}
+
+export function mapTermsState(state, map) {
+  return Object.assign(map, {
+    pullRocket: state.ui.pullRocket
+  })
+}
+
+
+const passProps = (uid, parentProps, props) => {
+  return Object.assign(props, {
+    pullRocket: parentProps.pullRocket
+  })
+}
+
+export { passProps as getTermGroupProps }
+export { passProps as getTermProps }
+
 export function decorateTerm(Term) {
   return class extends Component {
     constructor (props, context) {
       super(props,context)
-      
+      this.state = {
+        displayPullRocket: false
+      }
+    }
+
+    onTerminal(term) {
+      if (this.props.onTerminal) this.props.onTerminal(term);
+      this._div = term.div_;
+      this._window = term.document_.defaultView;
+    }
+
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.pullRocket === 0 || nextProps.pullRocket > this.props.pullRocket) {
+        this.setState({displayPullRocket: true})
+      } else {
+        this.setState({displayPullRocket: false})
+      }
+    }
+
+    onAnimationEnd(event) {
+      setTimeout(() => {
+          this.setState({
+          displayPullRocket: false
+        })
+      }, 1500)
     }
 
     render () {
       return( 
         <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-          <Spaceship display={true} onAnimationEnd={() => {return}}/>
+          <Spaceship display={this.state.displayPullRocket} onAnimationEnd={this.onAnimationEnd.bind(this)}/>
           {React.createElement(Term, Object.assign({}, this.props, {
             onTerminal: this._onTerminal,
           }))}
